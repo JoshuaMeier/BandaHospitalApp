82ff5eb1adf4dbfaec6bbfcc458060ef1266af21
jmblargent <jmblargent@gmail.com>
2017-09-19 16:26:36 -0500

    Added email functionality with the SendEmail class. Currently all of the error message does not send because it is not being read into the program correctly.

--- ParseLog.java
+++ ParseLog.java
@@ -3,28 +3,31 @@ import java.io.*; //IO functions
 public class ParseLog {
 	//This will be how logs are parsed into our program
 	public static void main(String[] args) {
-		for(int i=0;i<50;i++) parse();
-	}
 
-	//Setting up the File IO objects
-	static File file = new File("/Users/justinlargent/Downloads/catalina.out");
-	public static void parse() {
-		String[] split = null;
 		String log = null;
 		//reading a line from the log
 		try(InputStream fin = new FileInputStream(file);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(fin))) {
-			log = reader.readLine();
+			for(int i=0;i<100;i++) {
+				log = reader.readLine();
+				parse(log);
+			}
 			//System.out.println(log);
 		} catch (IOException ex) {
 			System.err.println(ex);
 		}
+	}
+
+	//Setting up the File IO objects
+	static File file = new File("/Users/justinlargent/Downloads/catalina.out");
+	public static void parse(String log) {
+		String[] split = null;
 
 		//splitting the log message into parts
 		split = log.split("[|]");
-		System.out.println(split[0]);
+		//System.out.println(split[1]);
 		//Determine if it is a Warning, Info, or Error
-		/*switch(split[0].charAt(0)) {
+		switch(split[0].charAt(0)) {
 			case 'W':
 				warn();
 				break;
@@ -32,9 +35,9 @@ public class ParseLog {
 				info();
 				break;
 			case 'E':
-				error();
+				error(split);
 				break;
-		}*/
+		}
 	}
 
 	public static void warn() {
@@ -45,8 +48,10 @@ public class ParseLog {
 		System.out.println("Information");
 	}
 
-	public static void error() {
+	public static void error(String[] split) {
+		String subject = split[0] + split[1];
+		SendEmail.send(split[2], subject);
 		System.out.println("Error");
 	}
-	
+
 }
