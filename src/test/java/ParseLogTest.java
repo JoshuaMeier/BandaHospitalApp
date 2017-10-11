ec4b7caf9943ea09a4fb3f89dafba5c120facb51
jmblargent <jmblargent@gmail.com>
2017-10-11 10:42:07 -0500

    Removing .idea folderfrom the repo since it is a IDE dependent folder.

--- src/test/java/ParseLogTest.java
+++ src/test/java/ParseLogTest.java
@@ -1,23 +1,5 @@
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class ParseLogTest {
-
-    ExceptionType exceptionList = new ExceptionType();
-    String string = "test|string";
-    ParseLog parser = new ParseLog(string, exceptionList);
-    @Test
-    public void parse() throws Exception {
-        parser.parse();
-        assertArrayEquals(string.split("[|]"), parser.split);
-    }
-
-    @Test
-    public void error() throws Exception {
-        parser.error(string.split("[|]"));
-        assertEquals(exceptionList, parser.exceptions);
-    }
 
 }
