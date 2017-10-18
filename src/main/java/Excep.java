32bc7a3ae3f26bd67fb851101cd5da92dd2b0c3d
Joshua Meier <joshuameier@letu.edu>
2017-10-18 10:37:49 -0500

    Wednesday changes

--- src/main/java/Excep.java
+++ src/main/java/Excep.java
@@ -1,15 +1,28 @@
 import java.util.LinkedList;
 
+/**
+ * This is the lower level class for the Banda hospital app data structure.
+ *      It has a lower level LinkedList that holds its data. Its also carries
+ *      the name of the Exception.
+ */
 public class Excep extends LinkedList{
     String nameOfExcep, dateAndTime;
 
     LinkedList times = new LinkedList<String>();
 
+    /**
+     *
+     */
     Excep() {
         nameOfExcep = "NewException";
         times.add("0000-00-00 00:00:00,000");
     }
 
+    /**
+     *
+     * @param dateAndTime
+     * @param nameOfExcep
+     */
     Excep(String dateAndTime, String nameOfExcep) {
         this.nameOfExcep = nameOfExcep;
         times.add(dateAndTime);
