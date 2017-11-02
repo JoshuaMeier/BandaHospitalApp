39cb8147d76ea64fa1f7f3de5dbf0c4dcd005aa1
Joshua Meier <joshuameier@letu.edu>
2017-11-01 22:42:46 -0500

    Added the test for Excep class.

--- src/main/java/Excep.java
+++ src/main/java/Excep.java
@@ -4,7 +4,11 @@ public class Excep {
     String message;
 
     Excep() {
+    }
 
+    Excep(String time, String message) {
+        this.time = time;
+        this.message = message;
     }
 
     public void add(String time, String message) {
--- src/main/java/ExcepTypes.java
+++ src/main/java/ExcepTypes.java
@@ -57,6 +57,5 @@ public class ExcepTypes {
         this.dateAndTime = dateAndTime;
     }
 
-
 }
 
